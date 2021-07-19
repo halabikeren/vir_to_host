c9466e37d88f4380c8ba7b14c633cc7d9c575db4
halabikeren <halabikeren@gmail.com>
2021-07-19 11:26:25 +0300

    altered collection of taxonomy data to work with the FTP dump files from NCBI

--- utils/data_collecting_utils.py
+++ utils/data_collecting_utils.py
@@ -1,4 +1,6 @@
 import logging
+
+logger = logging.getLogger(__name__)
 from enum import Enum
 import typing as t
 from collections import defaultdict
@@ -25,9 +27,7 @@ class RefSource(Enum):
     OTHER = 5
 
 
-def exit_handler(
-    df: pd.DataFrame, output_path: str, logger: logging.log, signum, frame
-):
+def exit_handler(df: pd.DataFrame, output_path: str, signum, frame):
     logger.error(
         f"closing on signal {signum} and saving temporary output to {output_path}"
     )
@@ -61,14 +61,17 @@ def parallelize_on_rows(
     )
 
 
-def handle_duplicated_columns(colname: str, df: pd.DataFrame, logger: logging.log):
+def handle_duplicated_columns(colname: str, df: pd.DataFrame):
     """
     :param colname: name of the column that was duplicated as a result of the merge
     :param df: dataframe to remove duplicated columns from
-    :param logger: logger instance
     :return:
     """
-    duplicated_columns = [col for col in df.columns if colname in col]
+    duplicated_columns = [
+        col for col in df.columns if colname in col and col != colname
+    ]
+    if len(duplicated_columns) == 0:
+        return
     main_colname = duplicated_columns[0]
     for col in duplicated_columns[1:]:
         # check that there are no contradictions
@@ -80,6 +83,9 @@ def handle_duplicated_columns(colname: str, df: pd.DataFrame, logger: logging.lo
             & (type(df[col].iloc[0]) not in [np.floating, float])
             & (df[main_colname] != df[main_colname])
         ]
+        contradictions = contradictions.dropna(
+            subset=[main_colname, col], how="any", inplace=False
+        )
         if contradictions.shape[0] > 0:
             logger.error(
                 f"Contradictions found between column values in {main_colname} and {col}"
@@ -119,18 +125,14 @@ def collect_dois(
     output_field_name: str,
     references_field: str,
     source_type: RefSource,
-    logger: logging.log,
     output_path: str,
-    ncpus: int = 1,
 ):
     """
     :param df: dataframe to add a references by DOIs columns to
     :param output_field_name: the name of the added field
     :param references_field: a list of fields by which be extracted
     :param source_type: the type of query to conduct to ge the doi
-    :param logger instance
     :param output_path: path to write temporary output to
-    :param ncpus: number of cpus available for run, in case of parallelization
     :return: none
     """
 
@@ -237,10 +239,9 @@ def collect_dois(
         logger.info(f"Processed DOI data for {num_records} records")
 
 
-def get_taxon_id(taxon_name: str, logger: logging.log) -> t.Optional[np.float64]:
+def get_taxon_id(taxon_name: str) -> t.Optional[np.float64]:
     """
     :param taxon_name: name of taxon
-    :param logger: logging instance
     :return: taxon id
     """
     try:
@@ -258,120 +259,196 @@ def get_taxon_id(taxon_name: str, logger: logging.log) -> t.Optional[np.float64]
         return np.nan
 
 
-def collect_taxonomy_data(
-    df: pd.DataFrame,
-    taxonomy_data_prefix: str,
-    output_dir: str,
-    logger: logging.log,
-    ncpus: int = 1,
-) -> pd.DataFrame:
+def collect_taxonomy_data(df: pd.DataFrame, taxonomy_data_dir: str,) -> pd.DataFrame:
     """
     :param df: dataframe holding taxon names (and possibly ids) by which taxon data should be extracted
-    :param taxonomy_data_prefix: prefix of taxon_name and taxon_id to search by and add new columns by (either "virus_" or "host_" in this context)
-    :param logger: logging instance
-    :param output_dir: directory to write temporary output to in case of signals
-    :param ncpus: number of required cpus
-    :return: the updated dataframe
+    :param taxonomy_data_dir: directory holding dump files of the NCBI taxonomy FTP services https://ftp.ncbi.nih.gov/pub/taxonomy/new_taxdump/
+    :return: the processed dataframe
     """
-    taxon_name_field = f"{taxonomy_data_prefix}_taxon_name"
-    taxon_id_field = f"{taxonomy_data_prefix}_taxon_id"
-    df.set_index(taxon_id_field)
-    i = 0
-    num_records = 0
-    batches_num = int((len(df.index) + 2) / 5)
-    if batches_num == 0:
-        batches_num += 1
-    for chunk in np.array_split(df, batches_num):
 
-        # define output path for signal handling
-        num_records += chunk.shape[0]
-        i += 1
-        output_path = f"{output_dir}/collect_taxonomy_data_chunk_{i}.csv"
-        signal.signal(
-            signal.SIGINT, partial(exit_handler, chunk, output_path),
+    df = df.applymap(lambda s: s.lower() if isinstance(s, str) else s)
+
+    # fill virus and host taxon ids
+    taxonomy_names_df = pd.read_csv(
+        f"{taxonomy_data_dir}/names.dmp",
+        sep="|",
+        header=None,
+        index_col=False,
+        names=["tax_id", "name_txt", "unique_name", "class_name"],
+    )
+    taxonomy_names_df.replace(to_replace="\t", value="", regex=True, inplace=True)
+    taxonomy_names_df.replace(to_replace="", value=np.nan, regex=True, inplace=True)
+    taxonomy_names_df = taxonomy_names_df.applymap(
+        lambda s: s.lower() if isinstance(s, str) else s
+    )
+    logger.info(
+        (
+            f"#missing virus taxon ids before addition= {df.loc[df.virus_taxon_id.isna()].shape[0]}"
         )
-        signal.signal(
-            signal.SIGTERM, partial(exit_handler, chunk, output_path),
+    )
+    virus_taxon_names_df = taxonomy_names_df.rename(
+        columns={"tax_id": "virus_taxon_id", "name_txt": "virus_taxon_name"}
+    )[["virus_taxon_id", "virus_taxon_name"]]
+    df = df.merge(virus_taxon_names_df, on="virus_taxon_name", how="left")
+    handle_duplicated_columns(colname="virus_taxon_id", df=df)
+    logger.info(
+        (
+            f"#missing virus taxon ids after addition = {df.loc[df.virus_taxon_id.isna()].shape[0]}"
         )
+    )
+    logger.info(
+        (
+            f"#missing host taxon ids before addition = {df.loc[df.host_taxon_id.isna()].shape[0]}"
+        )
+    )
+    host_taxon_names_df = taxonomy_names_df.rename(
+        columns={"tax_id": "host_taxon_id", "name_txt": "host_taxon_name"}
+    )[["host_taxon_id", "host_taxon_name"]]
+    df = df.merge(host_taxon_names_df, on="host_taxon_name", how="left")
+    handle_duplicated_columns(colname="host_taxon_id", df=df)
+    logger.info(
+        (
+            f"#missing host taxon ids after addition = {df.loc[df.host_taxon_id.isna()].shape[0]}"
+        )
+    )
 
-        try:
+    # fill in virus and host lineage info
+    taxonomy_lineage_df = pd.read_csv(
+        f"{taxonomy_data_dir}/rankedlineage.dmp",
+        sep="|",
+        header=None,
+        index_col=False,
+        names=[
+            "tax_id",
+            "tax_name",
+            "species",
+            "genus",
+            "family",
+            "order",
+            "class",
+            "phylum",
+            "kingdom",
+            "superkingdom",
+        ],
+        dtype=object,
+    )
+    taxonomy_lineage_df.replace(to_replace="\t", value="", regex=True, inplace=True)
+    taxonomy_lineage_df.replace(to_replace="", value=np.nan, regex=True, inplace=True)
+    taxonomy_lineage_df = taxonomy_lineage_df.applymap(
+        lambda s: s.lower() if isinstance(s, str) else s
+    )
 
-            # fill in missing taxon ids
-            taxa_name_to_id = pd.DataFrame(
-                {
-                    taxon_name_field: chunk.loc[
-                        pd.isnull(chunk[taxon_id_field]), taxon_name_field
-                    ].unique()
-                }
-            )
+    virus_taxonomy_lineage_df = taxonomy_lineage_df.rename(
+        columns={
+            col: f"virus_{col.replace('tax_id', 'taxon').replace('tax_name', 'taxon')}_{'id' if 'id' in col else 'name'}"
+            for col in taxonomy_lineage_df.columns
+        },
+    )
+    virus_taxonomy_lineage_df = virus_taxonomy_lineage_df.loc[
+        virus_taxonomy_lineage_df.virus_superkingdom_name == "viruses"
+    ]
+    host_taxonomy_lineage_df = taxonomy_lineage_df.rename(
+        columns={
+            col: f"host_{col.replace('tax_id', 'taxon').replace('tax_name', 'taxon')}_{'id' if 'id' in col else 'name'}"
+            for col in taxonomy_lineage_df.columns
+        },
+    )
+    host_taxonomy_lineage_df["host_is_mammalian"] = host_taxonomy_lineage_df[
+        "host_class_name"
+    ].apply(lambda x: 1 if x == "mammalia" else 0)
+    # by taxon id
+    df = df.merge(virus_taxonomy_lineage_df, on="virus_taxon_id", how="left")
+    for col in set([col.replace("_x", "").replace("_y", "") for col in df.columns]):
+        handle_duplicated_columns(colname=col, df=df)
+    # by taxon name
+    df = df.merge(virus_taxonomy_lineage_df, on="virus_taxon_name", how="left",)
+    for col in set([col.replace("_x", "").replace("_y", "") for col in df.columns]):
+        handle_duplicated_columns(colname=col, df=df)
+    # by species name
+    df = df.merge(virus_taxonomy_lineage_df, on="virus_species_name", how="left",)
+    for col in set([col.replace("_x", "").replace("_y", "") for col in df.columns]):
+        handle_duplicated_columns(colname=col, df=df)
+    # by taxon name
+    df = df.merge(host_taxonomy_lineage_df, on="host_taxon_id", how="left")
+    for col in set([col.replace("_x", "").replace("_y", "") for col in df.columns]):
+        handle_duplicated_columns(colname=col, df=df)
+    # by species name
+    df = df.merge(host_taxonomy_lineage_df, on="host_species_name", how="left",)
+    for col in set([col.replace("_x", "").replace("_y", "") for col in df.columns]):
+        handle_duplicated_columns(colname=col, df=df)
 
-            taxa_name_to_id[taxon_id_field] = taxa_name_to_id.apply(
-                func=lambda x: get_taxon_id(
-                    taxon_name=x[taxon_name_field], logger=logger,
-                ),
-                axis=1,
-            )
+    # fill in missing id data of lineage section names - this part failed
+    relevant_name_columns = [
+        col for col in df.columns if "_name" in col and "taxon" not in col
+    ]
+    for col in relevant_name_columns:
+        to_merge = taxonomy_names_df.rename(
+            columns={"tax_id": col.replace("_name", "_id")}
+        )
+        df = df.merge(to_merge, left_on=col, right_on="name_txt", how="left")
+        for c in set([c for c in df.columns if "_x" not in c and "_y" not in c]):
+            handle_duplicated_columns(colname=c, df=df)
+        # df.loc[
+        #     (df[col].notna()) & (taxonomy_names_df.name_txt.isin(df[col].unique())),
+        #     col.replace("_name", "_id"),
+        # ] = df.loc[
+        #     (df[col].notna()) & (taxonomy_names_df.name_txt.isin(df[col].unique())), col
+        # ].apply(
+        #     lambda x: taxonomy_names_df.loc[
+        #         taxonomy_names_df.name_txt == x, "tax_id"
+        #     ].values[0]
+        #     if taxonomy_names_df.loc[taxonomy_names_df.name_txt == x].shape[0] > 0
+        #     else np.nan
+        # )
 
-            logger.info(
-                f"Collected taxonomy ids for {num_records} records successfully on field {taxon_id_field}"
-            )
-
-            chunk.set_index(taxon_name_field, inplace=True)
-            taxa_name_to_id.set_index(taxon_name_field, inplace=True)
-            chunk.fillna(taxa_name_to_id, inplace=True)
-
-            # extract taxa data in batch
-            taxa_ids = [
-                str(int(item)) for item in chunk[taxon_id_field].dropna().unique()
-            ]
-            if len(taxa_ids) == 0:
-                continue
-
-            query = ",".join(taxa_ids)
-            try:
-                taxa_data = list(
-                    Entrez.read(
-                        Entrez.efetch(
-                            id=query,
-                            db="taxonomy",
-                            retmode="xml",
-                            retmax=len(taxa_ids),
-                        )
-                    )
-                )
-                for taxon_data in taxa_data:
-                    try:
-                        taxon_id = np.float64(taxon_data["TaxId"])
-                        chunk.loc[
-                            chunk[taxon_id_field] == taxon_id,
-                            f"{taxonomy_data_prefix}_taxon_rank",
-                        ] = taxon_data["Rank"]
-                        for item in taxon_data["LineageEx"]:
-                            chunk.loc[
-                                chunk[taxon_id_field] == taxon_id,
-                                f"{taxonomy_data_prefix}_{item['Rank']}_id",
-                            ] = np.int64(item["TaxId"])
-                            chunk.loc[
-                                chunk[taxon_id_field] == taxon_id,
-                                f"{taxonomy_data_prefix}_{item['Rank']}_name",
-                            ] = item["ScientificName"]
-                    except Exception as e:
-                        logger.error(
-                            f"Parsing of taxon data {taxon_data} failed due to error {e}"
-                        )
-                logger.info(
-                    f"Collected taxonomy data for {num_records} records successfully on ids of type {taxon_id_field}"
-                )
-            except Exception as e:
-                logger.error(
-                    f"batch query to taxonomy browser on {query} failed due to error {e}"
-                )
-        finally:
-            new_cols = [col for col in chunk.columns if col not in df.columns]
-            for col in new_cols:
-                df[col] = np.nan
-            df.set_index(taxon_name_field, inplace=True)
-            df.fillna(chunk, inplace=True)
-            df.reset_index(inplace=True)
+    # fill rank of virus and host taxa
+    taxonomy_ranks_df = pd.read_csv(
+        f"{taxonomy_data_dir}/nodes.dmp",
+        sep="|",
+        header=None,
+        index_col=False,
+        names=[
+            "tax_id",
+            "parent_tax_id",
+            "rank",
+            "embl_code",
+            "division_id",
+            "inherited_div_flag",
+            "genetic_code_id",
+            "inherited_GC_flag",
+            "mitochondrial_genetic_code_id",
+            "inherited_MGC_flag",
+            "GenBank_hidden_flag",
+            "hidden_subtree_root_flag",
+            "comments",
+            "plastid_genetic_code_id",
+            "inherited_PGC_flag",
+            "specified_species",
+            "hydrogenosome_genetic_code_id",
+            "inherited_HGC_flag",
+        ],
+    )
+    taxonomy_ranks_df = taxonomy_ranks_df.applymap(
+        lambda s: s.lower() if isinstance(s, str) else s
+    )
+    taxonomy_ranks_df.replace(to_replace="\t", value="", regex=True, inplace=True)
+    taxonomy_ranks_df.replace(to_replace="", value=np.nan, regex=True, inplace=True)
+    df.loc[
+        df.virus_taxon_id.isin(taxonomy_ranks_df.tax_id.unique()), "virus_taxon_rank"
+    ] = df.loc[
+        df.virus_taxon_id.isin(taxonomy_ranks_df.tax_id.unique()), "virus_taxon_id"
+    ].apply(
+        lambda x: taxonomy_ranks_df.loc[taxonomy_ranks_df.tax_id == x, "rank"]
+    )
+    df["virus_is_species"] = df["virus_taxon_rank"].apply(
+        lambda x: 1 if x == "species" else 0
+    )
+    df.loc[
+        df.host_taxon_id.isin(taxonomy_ranks_df.tax_id.unique()), "host_taxon_rank"
+    ] = df.loc[
+        df.host_taxon_id.isin(taxonomy_ranks_df.tax_id.unique()), "host_taxon_id"
+    ].apply(
+        lambda x: taxonomy_ranks_df.loc[taxonomy_ranks_df.tax_id == x, "rank"]
+    )
 
     return df
