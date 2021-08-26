9829c64c58cffed01fcf377f3e7b8ae7f8dafec0
halabikeren <halabikeren@gmail.com>
2021-08-26 12:53:49 +0300

    moved parallelized function to out-of-class scope

--- virus/collect_sequence_data.py
+++ virus/collect_sequence_data.py
@@ -4,6 +4,8 @@ import sys
 import logging
 from functools import partial
 
+from Bio import Entrez
+
 logger = logging.getLogger(__name__)
 
 import click
@@ -14,6 +16,79 @@ sys.path.append("..")
 from utils.sequence_utils import SequenceCollectingUtils
 from utils.parallelization_service import ParallelizationService
 
+
+def extract_missing_data_from_ncbi_api(df: pd.DataFrame, data_prefix: str, id_field: str) -> pd.DataFrame:
+    """
+    :param df: dataframe with items were all the fields except for the id field are missing
+    :param data_prefix: data prefix for each column in the dataframe
+    :param id_field: name of id field to be indexed, without the data prefix
+    :return: the dataframe with values from the ncbi api, if available
+    """
+    ids = list(df[f"{data_prefix}_{id_field}"].unique())
+    logger.info(f"complementing missing accession data from ncbi nucleotide api for {len(ids)} records")
+    id_to_raw_data = {name: Entrez.read(
+        Entrez.esearch(db="nucleotide", term=f"({name}[Organism]) AND complete genome[Text Word]", retmode="xml"))
+        for name in ids}
+    logger.info(f"data extraction from ncbi nucleotide api is complete")
+    id_to_gi_acc = {name: id_to_raw_data[name]['IdList'][0] for name in id_to_raw_data if
+                    len(id_to_raw_data[name]['IdList']) > 0}
+    df.set_index(f"{data_prefix}_{id_field}", inplace=True)
+    gi_accession_field_name = f"{data_prefix}_gi_accession"
+    if gi_accession_field_name not in df.columns:
+        df[gi_accession_field_name] = np.nan
+    df[gi_accession_field_name].fillna(value=id_to_gi_acc, inplace=True)
+    df.reset_index(inplace=True)
+
+    # do batch request on additional data
+    logger.info(
+        f"complementing missing sequence data from ncbi nucleotide api for {len(list(id_to_gi_acc.values()))} records")
+    ncbi_raw_data = list(
+        Entrez.parse(Entrez.efetch(dx="nucleotide", id=",".join(list(id_to_gi_acc.values())), retmode="xml")))
+    logger.info(f"data extraction from ncbi nucleotide api is complete")
+
+    # process raw data
+    logger.info(f"filling dataframe with complementary data from ncbi api")
+    gi_acc_to_raw_data = {
+        [item.split("|")[1] for item in ncbi_raw_data[i]['GBSeq_other-seqids'] if 'gi' in item][0]: ncbi_raw_data[i]
+        for i in range(len(ncbi_raw_data))}
+    gi_acc_to_refseq_acc = {
+        gi_acc: [item.split("|")[1] for item in gi_acc_to_raw_data[gi_acc]['GBSeq_other-seqids'] if 'ref' in item][
+            0] for gi_acc in gi_acc_to_raw_data if
+        len([item for item in gi_acc_to_raw_data[gi_acc]['GBSeq_other-seqids'] if 'ref' in item]) > 0}
+    gi_acc_to_genbank_acc = {
+        gi_acc: [item.split("|")[1] for item in gi_acc_to_raw_data[gi_acc]['GBSeq_other-seqids'] if 'gb' in item][0]
+        for gi_acc in gi_acc_to_raw_data if
+        len([item for item in gi_acc_to_raw_data[gi_acc]['GBSeq_other-seqids'] if 'gb' in item]) > 0}
+    gi_acc_to_refseq_seq = {gi_acc: gi_acc_to_raw_data[gi_acc]['GBSeq_sequence'] for gi_acc in gi_acc_to_refseq_acc}
+    gi_acc_to_refseq_cds = {gi_acc: ";".join(
+        [
+            feature["GBFeature_location"]
+            for feature in gi_acc_to_raw_data[gi_acc]["GBSeq_feature-table"]
+            if feature["GBFeature_key"] == "CDS"
+        ]
+    ) for gi_acc in gi_acc_to_refseq_acc}
+    gi_acc_to_genbank_seq = {gi_acc: gi_acc_to_raw_data[gi_acc]['GBSeq_sequence'] for gi_acc in
+                             gi_acc_to_genbank_acc}
+    gi_acc_to_genbank_cds = {gi_acc: ";".join(
+        [
+            feature["GBFeature_location"]
+            for feature in gi_acc_to_raw_data[gi_acc]["GBSeq_feature-table"]
+            if feature["GBFeature_key"] == "CDS"
+        ]
+    ) for gi_acc in gi_acc_to_genbank_acc}
+
+    # fill dataframe with the collected data
+    df.set_index(gi_accession_field_name, inplace=True)
+    df[f"{data_prefix}_refseq_accession"].fillna(value=gi_acc_to_refseq_acc, inplace=True)
+    df[f"{data_prefix}_refseq_sequence"].fillna(value=gi_acc_to_refseq_seq, inplace=True)
+    df[f"{data_prefix}_refseq_cds"].fillna(value=gi_acc_to_refseq_cds, inplace=True)
+    df[f"{data_prefix}_genbank_accession"].fillna(value=gi_acc_to_genbank_acc, inplace=True)
+    df[f"{data_prefix}_genbank_sequence"].fillna(value=gi_acc_to_genbank_seq, inplace=True)
+    df[f"{data_prefix}_genbank_cds"].fillna(value=gi_acc_to_genbank_cds, inplace=True)
+    df.reset_index(inplace=True)
+    logger.info(f"dataframe filling is complete")
+
+    return df
 
 @click.command()
 @click.option(
@@ -161,7 +236,7 @@ def collect_sequence_data(
         (virus_data["virus_refseq_sequence"].isna()) & (virus_data["virus_genbank_sequence"].isna())]
     virus_missing_data = ParallelizationService.parallelize(df=virus_missing_data,
                                                             func=partial(
-                                                                SequenceCollectingUtils.extract_missing_data_from_ncbi_api,
+                                                                extract_missing_data_from_ncbi_api,
                                                                 data_prefix="virus", id_field="taxon_name"),
                                                             num_of_processes=multiprocessing.cpu_count())
 
