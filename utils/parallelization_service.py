50008f870efb3a8c11bd6fc8980848928c266761
halabikeren <halabikeren@gmail.com>
2021-09-01 15:41:32 +0300

    updated service to remove subset paths after being used

--- utils/parallelization_service.py
+++ utils/parallelization_service.py
@@ -1,24 +1,30 @@
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 import pandas as pd
 import numpy as np
-#from pandas_msgpack import read_msgpack
+
+# from pandas_msgpack import read_msgpack
 
 from multiprocessing import Pool
 from functools import partial
 
 
 class ParallelizationService:
-
     @staticmethod
     def parallelize(
-            df: pd.DataFrame, func: partial.func, num_of_processes: int = 1,
+        df: pd.DataFrame,
+        func: partial.func,
+        num_of_processes: int = 1,
     ):
         df_split = np.array_split(df, num_of_processes)
         pool = Pool(num_of_processes)
-        df = pd.concat([pd.read_csv(path) for path in pool.map(partial(func), df_split)])
+        df_paths = [path for path in pool.map(partial(func), df_split)]
+        df = pd.concat([pd.read_csv(path) for path in df_paths])
+        for path in df_paths:
+            os.remove(path)
         pool.close()
         pool.join()
         return df
@@ -30,8 +36,12 @@ class ParallelizationService:
 
     @staticmethod
     def parallelize_on_rows(
-            df: pd.DataFrame, func: partial.func, num_of_processes: int = 1,
+        df: pd.DataFrame,
+        func: partial.func,
+        num_of_processes: int = 1,
     ):
         return ParallelizationService.parallelize(
-            df, partial(ParallelizationService.run_on_subset, func), num_of_processes,
+            df,
+            partial(ParallelizationService.run_on_subset, func),
+            num_of_processes,
         )
