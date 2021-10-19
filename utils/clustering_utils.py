c00f802c06d606e84c1a2cfc45bba6657c5ca753
halabikeren <halabikeren@gmail.com>
2021-10-19 20:47:45 +0300

    added condition to avoid computing centroid for a cluster of size 1

--- utils/clustering_utils.py
+++ utils/clustering_utils.py
@@ -196,18 +196,20 @@ class ClusteringUtils:
             pairwise_similarities_df = ClusteringUtils.get_pairwise_similarities_df(
                 input_path=data_path.replace("_aligned.fasta", "_similarity_values.csv")
             )
-            outliers_idx = ClusteringUtils.compute_outliers_with_euclidean_dist(
-                data=pairwise_similarities_df[
-                    [
-                        col
-                        for col in pairwise_similarities_df.columns
-                        if "similarity_to" in col
-                    ]
-                ],
-                data_dist_plot_path=data_path.replace(
-                    "_aligned.fasta", "_euclidean.jpeg"
-                ),
-            )
+            outliers_idx = []
+            if pairwise_similarities_df.shape[0] > 1:
+                outliers_idx = ClusteringUtils.compute_outliers_with_euclidean_dist(
+                    data=pairwise_similarities_df[
+                        [
+                            col
+                            for col in pairwise_similarities_df.columns
+                            if "similarity_to" in col
+                        ]
+                    ],
+                    data_dist_plot_path=data_path.replace(
+                        "_aligned.fasta", "_euclidean.jpeg"
+                    ),
+                )
         accessions = list(data.accession)
         accessions_to_keep = [
             accessions[idx] for idx in range(len(accessions)) if idx not in outliers_idx
