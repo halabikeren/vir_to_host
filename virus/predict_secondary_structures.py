4717c282f7d61321c5acc42cb69addbd655e0350
halabikeren <halabikeren@gmail.com>
2021-11-21 12:18:02 +0200

    excluded single sequence species from the pipeline

--- virus/predict_secondary_structures.py
+++ virus/predict_secondary_structures.py
@@ -1,4 +1,8 @@
 import logging
+from datetime import time
+
+import numpy as np
+from Bio import SeqIO
 
 logger = logging.getLogger(__name__)
 
@@ -59,57 +63,49 @@ def get_secondary_struct(
             struct_conservation_index,
         )
 
-    os.makedirs(workdir, exist_ok=True)
-    rnalalifold_output_dir = f"{workdir}/RNALalifold/"
-    os.makedirs(rnalalifold_output_dir, exist_ok=True)
-    res = RNAPredUtils.exec_rnalalifold(
-        input_path=sequence_data_path, output_dir=rnalalifold_output_dir
-    )
-    mlocarna_input_dir = f"{workdir}/mLocaRNA/input/"
-    os.makedirs(mlocarna_input_dir, exist_ok=True)
-    RNAPredUtils.parse_rnaalifold_output(
-        rnaalifold_output_dir=rnalalifold_output_dir,
-        mlocarna_input_dir=mlocarna_input_dir,
-    )
-    mlocarna_output_dir = f"{workdir}/mLocaRNA/output/"
-    os.makedirs(mlocarna_output_dir, exist_ok=True)
-    os.makedirs(mlocarna_output_dir, exist_ok=True)
-    for path in os.listdir(mlocarna_input_dir):
-        res = RNAPredUtils.exec_mlocarna(
-            input_path=f"{mlocarna_input_dir}{path}",
-            output_path=f"{mlocarna_output_dir}{path.replace('.fasta', '_mlocrna.out')}",
-        )
-
-    rnaz_input_dir = f"{workdir}/RNAz/input/"
-    os.makedirs(rnaz_input_dir, exist_ok=True)
-    for path in os.listdir(mlocarna_output_dir):
-        RNAPredUtils.parse_mlocarna_output(
-            mlocarna_output_path=f"{mlocarna_output_dir}{path}",
-            rnaz_input_path=f"{rnaz_input_dir}{path.replace('_mlocrna.out', '_rnaz.in')}",
-        )
-
-    rnaz_output_dir = f"{workdir}/RNAz/output/"
-    os.makedirs(rnaz_output_dir, exist_ok=True)
-    for path in os.listdir(rnaz_input_dir):
-        res = RNAPredUtils.exec_rnaz(
-            input_path=f"{rnaz_input_dir}{path}",
-            output_path=f"{rnaz_output_dir}{path.replace('_rnaz.in', 'rnaz.out')}",
-        )
-
+    num_sequences = len(list(SeqIO.parse(sequence_data_path, format="fasta")))
     secondary_structures = []
-    for path in os.listdir(rnaz_output_dir):
-        secondary_structures.append(
-            RNAPredUtils.parse_rnaz_output(
-                rnaz_output_path=f"{rnaz_output_dir}{path}",
-                significance_score_cutoff=significance_score_cutoff,
-            )
-        )
+    if num_sequences > 1:
+        logger.info(f"computing rnaz reliable windows for prediction")
+        rnaz_window_output_path = f"{workdir}/rnaz_window.out"
+        RNAPredUtils.execute_rnaz_window(input_path=sequence_data_path, output_path=rnaz_window_output_path)
+        logger.info(f"executing RNAz predictor on refined windows")
+        rnaz_output_path = f"{workdir}/rnaz_initial.out"
+        RNAPredUtils.exec_rnaz(input_path=rnaz_window_output_path, output_path=rnaz_output_path)
+        logger.info(f"clustering RNAz hits of overlapping windows")
+        rnaz_cluster_output_path = f"{workdir}/rnaz_cluster.dat"
+        RNAPredUtils.exec_rnaz_cluster(input_path=rnaz_output_path, output_path=rnaz_cluster_output_path)
+        logger.info(f"extracting sequence data per selected window for mlocarna refinement")
+        rnaz_candidates_output_dir = f"{workdir}/rnaz_candidates_sequence_data/"
+        RNAPredUtils.parse_candidates(candidates_info_path=rnaz_cluster_output_path, sequence_data_path=rnaz_window_output_path, output_dir=rnaz_candidates_output_dir)
+        logger.info(f"creating refined alignments of candidates with mlocarna")
+        mlocarna_output_dir = f"{workdir}/rnaz_candidates_mlocarna_aligned/"
+        for path in os.listdir(rnaz_candidates_output_dir):
+            input_path = f"{rnaz_candidates_output_dir}{path}"
+            output_path = f"{mlocarna_output_dir}{path.replace('.fasta', '.clustal')}"
+            RNAPredUtils.exec_mlocarna(input_path=input_path, output_path=output_path)
+        logger.info(f"executing prediction on aligned windows with rnaz to be able to classify the selected structures")
+        rnaz_refined_output_dir = f"{workdir}/rnaz_final_output/"
+        os.makedirs(rnaz_refined_output_dir, exist_ok=True)
+        for path in os.listdir(mlocarna_output_dir):
+            input_path=f"{mlocarna_output_dir}{path}"
+            output_path = f"{rnaz_refined_output_dir}{path.replace('.clustal', '_rnaz.out')}"
+            RNAPredUtils.exec_rnaz(input_path=input_path, output_path=output_path)
+        logger.info(f"parsing the obtained rna structures")
+        for path in os.listdir(rnaz_refined_output_dir):
+            struct = RNAPredUtils.parse_rnaz_output(rnaz_output_path=f"{rnaz_refined_output_dir}{path}")
+            secondary_structures.append(struct)
+    else:
+        # use 1) RNALfold with cutoff that is based on RNAz results for structures predicted for species with more than 1 structure
+        pass
 
-    for struct in secondary_structures:
+    functional_structures = [struct for struct in secondary_structures if struct.is_significant and struct.is_functional_structure]
+    logger.info(f"out of {len(secondary_structures)}, {len(functional_structures)} are significant and functional")
+    logger.info(f"the mean z-score for the predicted structures is {np.mean([struct.mean_zscore for struct in functional_structures])} and standard deviation of {np.std([struct.mean_zscore for struct in functional_structures])}")
+    for struct in functional_structures: # or should I use functional_structures
         struct_representation.append(struct.consensus_representation)
         struct_sequence.append(struct.consensus_sequence)
         struct_prob.append(struct.svm_rna_probability)
-        struct_significance.append(struct.is_significant)
         struct_mfe.append(struct.consensus_mfe)
         struct_entropy.append(struct.shannon_entropy)
         struct_conservation_index.append(struct.structure_conservation_index)
@@ -126,25 +122,23 @@ def get_secondary_struct(
 
 
 def compute_rna_secondary_structures(
-    clusters_df: pd.DataFrame,
-    cluster_field_name: str,
+    input_df: pd.DataFrame,
     sequence_data_dir: str,
     workdir: str,
     output_path: str,
     significance_score_cutoff: float = 0.9,
 ):
     """
-    :param clusters_df:
-    :param cluster_field_name:
-    :param sequence_data_dir:
-    :param workdir:
+    :param input_df: dataframe with viral species of interest
+    :param sequence_data_dir: directory holding sequence data of the viral species of interest
+    :param workdir: directory to
     :param output_path:
     :param significance_score_cutoff: significance_score_cutoff: threshold between 0 and 1 determining the cutoff of secondary structure RNAz
     probability based on which the structure will be determined as significant or not
     :return:
     """
     secondary_structures_df = pd.DataFrame(
-        {cluster_field_name: clusters_df[cluster_field_name].unique()}
+        {"virus_species_name": input_df["virus_species_name"].unique()}
     )
     secondary_struct_fields = [
         "struct_representation",
@@ -156,11 +150,11 @@ def compute_rna_secondary_structures(
         "struct_conservation_index",
     ]
     secondary_structures_df[secondary_struct_fields] = secondary_structures_df.groupby(
-        cluster_field_name, group_keys=False
+        "virus_species_name", group_keys=False
     ).apply(
         lambda group: get_secondary_struct(
-            sequence_data_path=f"{sequence_data_dir}{re.sub('[^0-9a-zA-Z]+', '_', group.irow(0)[cluster_field_name])}_aligned.fasta",
-            workdir=f"{workdir}/{re.sub('[^0-9a-zA-Z]+', '_', group.irow(0)[cluster_field_name])}/",
+            sequence_data_path=f"{sequence_data_dir}{re.sub('[^0-9a-zA-Z]+', '_', group.irow(0)['virus_species_name'])}_aligned.fasta",
+            workdir=f"{workdir}/{re.sub('[^0-9a-zA-Z]+', '_', group.irow(0)['virus_species_name'])}/",
             significance_score_cutoff=significance_score_cutoff,
         )
     )
@@ -170,16 +164,9 @@ def compute_rna_secondary_structures(
 
 @click.command()
 @click.option(
-    "--associations_clusters_data_path",
+    "--associations_data_path",
     type=click.Path(exists=True, file_okay=True, readable=True),
-    help="input path, holding associations grouped by some rational",
-)
-@click.option(
-    "--cluster_field_name",
-    type=str,
-    help="name of id of clusters, corresponding to unique cluster values",
-    required=False,
-    default="virus_species_name",
+    help="input path of associations grouped viral species and host species",
 )
 @click.option(
     "--sequence_data_dir",
@@ -205,10 +192,11 @@ def compute_rna_secondary_structures(
     "--significance_score_cutoff",
     type=click.FloatRange(min=0, max=1),
     help="significance_score_cutoff: threshold between 0 and 1 determining the cutoff of secondary structure RNAz probability based on which the structure will be determined as significant or not",
+    required=False,
+    default = 0.9,
 )
 def compute_seq_similarities(
-    associations_clusters_data_path: click.Path,
-    cluster_field_name: str,
+    associations_data_path: click.Path,
     sequence_data_dir: click.Path,
     workdir: click.Path,
     log_path: click.Path,
@@ -226,8 +214,7 @@ def compute_seq_similarities(
     )
 
     compute_rna_secondary_structures(
-        clusters_df=pd.read_csv(associations_clusters_data_path),
-        cluster_field_name=cluster_field_name,
+        input_df=pd.read_csv(associations_data_path),
         sequence_data_dir=str(sequence_data_dir),
         workdir=str(workdir),
         output_path=str(df_output_path),
