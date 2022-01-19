896a91eb342851b18d618016fe35c26784f9b92a
halabikeren <halabikeren@gmail.com>
2022-01-19 08:40:18 +0200

    black

--- virus/categorize_secondary_structures.py
+++ virus/categorize_secondary_structures.py
@@ -1,4 +1,5 @@
 from pandarallel import pandarallel
+
 pandarallel.initialize()
 
 import click
@@ -22,11 +23,7 @@ logger = logging.getLogger(__name__)
     help="path to dataframe holding the names of species for which data should be collected. this dataframe is  expected to hold both the virus_species_name and the grouping_field in it",
 )
 @click.option(
-    "--grouping_field",
-    type=str,
-    help="column name to group data by",
-    required=False,
-    default="virus_family_name",
+    "--grouping_field", type=str, help="column name to group data by", required=False, default="virus_family_name",
 )
 @click.option(
     "--sequence_data_dir",
@@ -43,7 +40,7 @@ logger = logging.getLogger(__name__)
     type=click.Choice(["range", "annotation"]),
     help="indicator weather tp partition the data by constant ranges across the genome or by annotations (UTRs / CDS / ect.)",
     required=False,
-    default="annotation"
+    default="annotation",
 )
 @click.option(
     "--partition_size",
@@ -55,8 +52,8 @@ logger = logging.getLogger(__name__)
 @click.option(
     "--vadr_annotation_path",
     type=click.Path(exists=True, file_okay=True, readable=True),
-    help="path to vadr annotation path, with the annotations of accessions. "
-         "file suffix should be .ftr according to https://github.com/ncbi/vadr/blob/master/documentation/formats.md#annotate",
+    help="path to annotations inspired by vadr, already processed in the form of a csv file"
+    "file suffix should be .ftr according to https://github.com/ncbi/vadr/blob/master/documentation/formats.md#annotate",
     required=False,
     default=None,
 )
@@ -64,19 +61,19 @@ logger = logging.getLogger(__name__)
     "--workdir",
     type=click.Path(exists=False, file_okay=True, readable=True),
     help="directory to write family sequence data and align it in",
-    required=True
+    required=True,
 )
 @click.option(
     "--log_path",
     type=click.Path(exists=False, file_okay=True, readable=True),
     help="path holding the logging of the script",
-    required=True
+    required=True,
 )
 @click.option(
     "--df_output_path",
     type=click.Path(exists=False, file_okay=True, readable=True),
     help="path holding the output dataframe to write",
-    required=True
+    required=True,
 )
 def partition_secondary_structures(
     secondary_structures_df_path: click.Path,
@@ -95,10 +92,7 @@ def partition_secondary_structures(
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s module: %(module)s function: %(funcName)s line: %(lineno)d %(message)s",
-        handlers=[
-            logging.StreamHandler(sys.stdout),
-            logging.FileHandler(str(log_path)),
-        ],
+        handlers=[logging.StreamHandler(sys.stdout), logging.FileHandler(str(log_path)),],
         force=True,  # run over root logger settings to enable simultaneous writing to both stdout and file handler
     )
 
@@ -106,7 +100,9 @@ def partition_secondary_structures(
     secondary_structures_df = pd.read_csv(secondary_structures_df_path)
     logger.info(f"applying partitioning pipeline for {secondary_structures_df.shape[0]} structures")
     if grouping_field not in secondary_structures_df.columns:
-        error = f"grouping field {grouping_field} is absent from the dataframe columns at {secondary_structures_df_path}"
+        error = (
+            f"grouping field {grouping_field} is absent from the dataframe columns at {secondary_structures_df_path}"
+        )
         logger.error(error)
         raise ValueError(error)
     if "virus_species_name" not in secondary_structures_df.columns:
@@ -118,17 +114,19 @@ def partition_secondary_structures(
     logger.info(f"dataframe consists of {len(secondary_structures_groups.groups.keys())} groups of {grouping_field}")
     output_dfs = []
     for g in secondary_structures_groups.groups.keys():
-        if g == "flaviviridae": # remove after analysis to generalize to all viral families
+        if g == "flaviviridae":  # remove after analysis to generalize to all viral families
 
             logger.info(f"handling group {g}")
             df = secondary_structures_groups.get_group(g)
 
             # map the start position of each secondary structure from its original value (determined by the species-wise alignment) to its new value (determined by the family-wise alignment) in the output df
             logger.info(f"mapping species-wise structures positions to group-wise positions")
-            df = RNAStructUtils.map_species_wise_pos_to_group_wise_pos(df=df,
-                                                   seq_data_dir=sequence_data_dir,
-                                                   species_wise_msa_dir=species_wise_msa_dir,
-                                                   workdir=f"{workdir}/{g}/")
+            df = RNAStructUtils.map_species_wise_pos_to_group_wise_pos(
+                df=df,
+                seq_data_dir=sequence_data_dir,
+                species_wise_msa_dir=species_wise_msa_dir,
+                workdir=f"{workdir}/{g}/",
+            )
 
             # obtain the annotations of each species in the family, and the range of each annotation within the representative genome (throw away annotations that do not appear in all the species within the family)
             logger.info(f"assigning partitions to structures based on {partition_by}")
@@ -142,5 +140,5 @@ def partition_secondary_structures(
     output_df.to_csv(df_output_path, index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     partition_secondary_structures()
